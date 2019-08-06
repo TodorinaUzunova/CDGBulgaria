e0f1163dd5c68ee9a0ad92ee59ebaf5a9f704449
TodorinaUzunova <toschkam@abv.bg>
2019-08-06 14:46:47 +0300

    set validation to CDGUser

--- Data/CSGBulgaria.Data.Models/CDGUser.cs
+++ Data/CSGBulgaria.Data.Models/CDGUser.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CDGBulgaria.Data.Models
 {
 	public class CDGUser : IdentityUser<string>
 	{
-
+		[Required]
+		[MaxLength(60)]
 		public string FullName { get; set; }	
 	
 	}
