72ebf28aa50cbe502dd5fb77e450fb35700e0842
TodorinaUzunova <toschkam@abv.bg>
2019-08-20 22:59:38 +0300

    corrected tests for questionsService

--- Tests/CDGBulgariaTests/Service/QuestionsServiceTests.cs
+++ Tests/CDGBulgariaTests/Service/QuestionsServiceTests.cs
@@ -20,8 +20,7 @@ namespace CDGBulgariaTests.Service
 	public class QuestionsServiceTests
 	{
 		private IQuestionsService questionsService;
-		private IHttpContextAccessor contextAccessor;
-
+	
 		public QuestionsServiceTests()
 		{
 			MapperInitializer.InitializeMapper();
@@ -33,13 +32,11 @@ namespace CDGBulgariaTests.Service
 			return new List<Question> {
 				new Question {
 					Content="Is this is a new kind of rare desease?",
-					AuthorId="cvthsg12lkmm",
-					CreatedOn=DateTime.UtcNow
+					Author= new CDGUser{UserName="pesho" },
 				  },
 				new Question {
 					Content="What happens, when the children have this desease?",
-					AuthorId="cvthsg12lkmm3456",
-					CreatedOn=DateTime.UtcNow
+					Author= new CDGUser{UserName="todor" },
 				  },
 			};
 		}
@@ -60,9 +57,7 @@ namespace CDGBulgariaTests.Service
 			var context = CDGBulgariaInmemoryFactory.InitializeContext();
 
 			await SeedData(context);
-
-			this.contextAccessor = new HttpContextAccessor();
-			this.questionsService = new QuestionsService(context, contextAccessor);
+			this.questionsService = new QuestionsService(context);
 
 			List<QuestionServiceModel> actualResults = await this.questionsService.GetAllQuestions().ToListAsync();
 
@@ -74,8 +69,7 @@ namespace CDGBulgariaTests.Service
 				var actualEntry = actualResults[i];
 
 				Assert.True(expectedEntry.Content == actualEntry.Content, errorMessagePrefix + " " + "Content is not returned properly");
-				Assert.True(expectedEntry.CreatedOn == actualEntry.CreatedOn, errorMessagePrefix + " " + "CreatedOn is not returned properly");
-				Assert.True(expectedEntry.AuthorId == actualEntry.AuthorId, errorMessagePrefix + " " + "AuthorId is not returned properly");
+				Assert.True(expectedEntry.Author.UserName == actualEntry.Author.UserName, errorMessagePrefix + " " + "AuthorUserName is not returned properly");
 			}
 
 		}
@@ -87,9 +81,7 @@ namespace CDGBulgariaTests.Service
 			string errorMessagePrefix = "QuestionsService Method GetAllQuestions() does not work properly.";
 
 			var context = CDGBulgariaInmemoryFactory.InitializeContext();
-
-			this.contextAccessor = new HttpContextAccessor();
-			this.questionsService = new QuestionsService(context, contextAccessor);
+			this.questionsService = new QuestionsService(context);
 
 			List<QuestionServiceModel> actualResults = await this.questionsService.GetAllQuestions().ToListAsync();
 
@@ -104,18 +96,16 @@ namespace CDGBulgariaTests.Service
 			string errorMessagePrefix = "QuestionsService Method CreateQuestion() does not work properly.";
 
 			var context = CDGBulgariaInmemoryFactory.InitializeContext();
+			this.questionsService = new QuestionsService(context);
 
-			this.contextAccessor = new HttpContextAccessor();
-			this.questionsService = new QuestionsService(context, contextAccessor);
-
-		 QuestionServiceModel questio= new QuestionServiceModel()
+		 QuestionServiceModel questionModel= new QuestionServiceModel()
 			{
 				Content = "Are there really found medicines for this desease?",
-				AuthorId="sdwrefghjk12345",
+				Author= new CDGUserServiceModel { UserName = "pesho" },
 				CreatedOn= DateTime.Parse("10/07/2019 10:30"),
 			};
 			
-			bool actualResult = await this.questionsService.Create(questio);
+			bool actualResult = await this.questionsService.Create(questionModel);
 
 			Assert.True(actualResult, errorMessagePrefix);
 
